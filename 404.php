ca3988387beeee3030244bd504a9a2b84021c2d7
Jutta Hyrskylahti <jutta.hyrskylahti@gmail.com>
2020-06-20 09:36:58 +0300

    added error page template and styles to the site

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<main class="basic-page">
+
+  <?php get_template_part('template-parts/section','errorpage'); ?>
+
+</main>
+
+<?php get_footer(); ?>
---
+++ template-parts/section-errorpage.php
@@ -0,0 +1,10 @@
+<div class="wider-at-right--top">
+  <div class="inner-content center-text">
+    <h1 class="page-title"><?php _e( 'Sorry, but the content was not found', 'portfolio' ); ?></h1>
+    <figure class="error-image"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/error-page.png" alt="Content was not found"></figure>
+    <p>
+      <?php _e( 'Perhaps go back to the home page?', 'portfolio' ); ?>
+      <a href="<?php echo get_home_url(); ?>"><?php _e( 'Go home >', 'portfolio' ); ?></a>
+    </p>
+  </div>
+</div>
